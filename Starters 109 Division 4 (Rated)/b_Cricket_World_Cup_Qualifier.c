56e0498b075f2102a4351562d1fff6a187118b3e
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2023-12-02 11:38:42 +0530

    Create b_Cricket_World_Cup_Qualifier.c
    The cricket World Cup has started in Chefland. There are many teams participating in the group stage matches. Any team that scores 12 or more points in the group stage matches qualifies for the next stage.
    You know the score that a particular team has scored in the group stage matches. Determine if the team has qualified for the next stage or not.

---
+++ Starters 109 Division 4 (Rated)/b_Cricket_World_Cup_Qualifier.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+	int X;
+	scanf("%d",&X);
+	if ((X>=1)&&(X<=20))
+	{
+	    if (X>=12)
+	    {
+	        printf("Yes");
+	    }
+	    else 
+	    {
+	        printf("No");
+	    }
+	}
+	return 0;
+}
+
