5a445594ab69a781fd3b3662cb7e6264c594a22a
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-03 22:17:23 +0530

    Create Christmas_Greetings.c
    Christmas is celebrated on 25th December every year. You are given a day X in December. Determine whether it is Christmas. Print CHRISTMAS if it is Christmas. Otherwise print ORDINARY.

---
+++ Starters 114 Division 4 (Rated) /Christmas_Greetings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int x;
+	scanf("%d",&x);
+    if (x==25)
+    {
+        printf("CHRISTMAS\n");
+    }
+    else
+    {
+        printf("ORDINARY\n");
+    }
+}
