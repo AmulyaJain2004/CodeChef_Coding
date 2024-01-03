99bea41a434e69a7f487354bb9642a1fe185be78
Amulya Jain <143023082+AmulyaJain2004@users.noreply.github.com>
2024-01-03 22:20:18 +0530

    Create Codechef_Round.c
    Codechef rounds are held on every Wednesday now, and not on any other days. You are wondering whether there is a Codechef round today. You know it is the Nth  day of the week (Sunday is 1st, Monday is 2nd, Tuesday is 3rd, Wednesday is 4th and so on). Can you tell whether there is a Codechef round today?

---
+++ Starters 115 Division 4 (Rated)/Codechef_Round.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int N;
+	scanf("%d",&N);
+	if (N==4)
+	{
+	    printf("YES\n");
+	}
+	else
+	{
+	    printf("NO\n");
+	}
+    return 0;
+}
